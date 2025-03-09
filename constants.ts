f1eea0d25067dfa94818d1ff3657a3f3b2738c1e
Jade Clyne <krclyne@gmail.com>
2025-03-08 21:10:13 -0700

    Changing DEFAULT_COUNCIL_ADDRESS to Kessenuma Council deployment

--- constants.ts
+++ constants.ts
@@ -3,7 +3,7 @@ export const SITE_DESCRIPTION =
   "Democratically allocate a budget across projects";
 
 export const DEFAULT_COUNCIL_ADDRESS =
-  "0xfa942226e5dd1e2d4d99014982846786b09939da";
+  "0xE623fc858456F8A1FbdB81aba5Fc8E746907CF10";
 export const COUNCIL_FACTORY_ADDRESS =
   "0xdef306E171B39101cf133747f391E5e1A49C8948"; // Update also in contracts/councilhaus/subgraph/config/*.json
 export const NETWORK: "optimism" | "base" = "base";
